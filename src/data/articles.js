a4a7e76e8e1a4ff791dfb5581884fc07456154af
Gobinath B <princegopinath42@gmail.com>
2024-03-08 12:34:41 +0530

    added article

--- src/data/articles.js
+++ src/data/articles.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-function article_1() {
+function article_2() {
 	return {
 		date: "16 Sept 2023",
 		title: "Pushing Limits: How We Triumphed in a 24-Hour Hackathon at SNS College of Technology",
+		link: "https://www.linkedin.com/pulse/pushing-limits-how-we-triumphed-24-hour-hackathon-sns-boopathiraj/",
 		description:
 			"In the world of technology, innovation and determination often go hand in hand. The ability to push one's limits and tackle complex challenges head-on is what sets exceptional individuals apart from the rest. This is the story of how a team of dedicated individuals, including myself, conquered the odds and emerged victorious in a grueling 24-hour hackathon at SNS College of Technology.",
 		keywords: [
@@ -40,10 +41,11 @@ function article_1() {
 	};
 }
 
-function article_2() {
+function article_3() {
 	return {
 		date: "28 July 2023",
 		title: "DevSecOps: Enhancing Security in the Development Process",
+		link: "https://www.linkedin.com/pulse/devsecops-enhancing-security-development-process-gobinath-boopathiraj/",
 		description:
 			"DevSecOps, a term derived from the combination of Development, Security, and Operations, has gained significant traction in the software industry. It represents a cultural shift towards integrating security practices into the software development process from the very beginning. ",
 		style: ``,
@@ -66,7 +68,34 @@ function article_2() {
 		),
 	};
 }
+function article_1() {
+	return {
+		date: "05 March 2024",
+		title: "Unveiling the Mathematical Odyssey: My Journey into Machine Learning",
+		link: "https://www.linkedin.com/pulse/unveiling-mathematical-odyssey-my-journey-machine-boopathiraj-ff1qc/?trackingId=3oyAi6q%2FT4CHe%2FdmFmHvkg%3D%3D",
+		description:
+			"Embarking on the path of machine learning, I envisaged diving into a realm of algorithms, data, and cutting-edge technology. However, what I soon discovered was that beneath the surface of this captivating field lies a profound reliance on mathematical concepts.",
+		style: ``,
+		keywords: [
+			"Machine Learning",
+			"Mathematics",
+			"Gobinath",
+			"Gobinath B",
+			"Gobinath Boopathiraj",
+		],
+		body: (
+			<React.Fragment>
+				<h1> my learning journey traversed through the corridors of mathematics, illuminating the intricate foundations of machine learning.</h1>
 
-const myArticles = [article_1, article_2];
+				<img
+						src="../ml.png"
+						alt="random"
+						className="randImage"
+					/>
+			</React.Fragment>
+		),
+	};
+}
+const myArticles = [article_1, article_2, article_3];
 
 export default myArticles;
