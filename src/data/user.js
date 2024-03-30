d07049a76c6284b2bed7721c6012d60a6dd01290
Gobinath B <princegopinath42@gmail.com>
2024-03-30 23:33:29 +0530

    refactor

--- src/data/user.js
+++ src/data/user.js
@@ -16,7 +16,7 @@ const INFO = {
 	},
 
 	homepage: {
-		title: "Full-stack web developer and Amateur infosec researcher",
+		title: "Software Engineer and Amateur Infosec Researcher",
 		description:
 			"I am Gobinath aka Prince,an aspiring MERN stack web developer with expertise in MongoDb, ExpressJs, ReactJs, NodeJs. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
 	},
