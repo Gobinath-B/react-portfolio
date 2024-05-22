c5a572fa6a6412e574226c79f59c5a7a50765555
Gobinath B <princegopinath42@gmail.com>
2024-05-22 20:27:26 +0530

    chore

--- src/data/user.js
+++ src/data/user.js
@@ -18,7 +18,7 @@ const INFO = {
 	homepage: {
 		title: "Software Engineer and Amateur Infosec Researcher",
 		description:
-			"I am Gobinath aka Prince,an aspiring MERN stack web developer with expertise in MongoDb, ExpressJs, ReactJs, NodeJs. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+			"I am Gobinath aka Prince,an aspiring MERN stack web developer with expertise in MongoDb, ExpressJs, ReactJs, NodeJs. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.Done Open Source Contribution in Microsoft DevHome and HacktoberFest.",
 	},
 
 	about: {
